6f8e56309cccc1cc5f7efc58e948ed57bd9b6fc3
prof. Frank Emanuel <emanuef@algonquincollege.com>
2023-07-26 16:45:13 -0400

    full colour all the sugar

---
+++ colour.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+for ii in {0..7}
+do
+    for i in {0..7}
+    do
+        tput setaf $i
+        tput setab $ii
+        echo -n "This colour is $i"
+
+        # no sugar!
+
+        tput sgr0
+        echo
+    done
+done
+
+echo
+
+tput setab 0
+tput setaf 7
+
+echo
+echo hello there people
+echo
+
+tput bold
+
+echo -n hello there people
+
+tput sgr0
+
+echo
+
+echo all done.
+
+echo
+
