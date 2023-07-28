9479aafa879417d44b697b4bb38c002e8786b4d7
prof. Frank Emanuel <emanuef@algonquincollege.com>
2023-07-28 13:35:46 -0400

    getting ready to mark

--- labtester.sh
+++ labtester.sh
@@ -18,5 +18,15 @@ echo "test 4: alternate length"
 
 $1 "this is my message!"
 
+echo "test 5: checking one option"
+
+$1 -w30 "george rules"
+
+echo "test 6: two options"
+
+$1 -p2 -c "george really rules"
+
+
+
 
 
